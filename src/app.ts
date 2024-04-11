c574701f806cf8a884a4becd6923b4b751379628
manmohanbcs11 <manmohanbcs11@gmail.com>
2024-04-11 19:46:12 +0530

    added cors library to allow frontend to make request to backend

--- src/app.ts
+++ src/app.ts
@@ -2,12 +2,14 @@ import dotenv from 'dotenv';
 import express from 'express';
 import authRouter from './routes/auth';
 import notesRouter from './routes/notes';
+import cors from 'cors';
 
 dotenv.config();
 
 const app = express();
 const port = 5000;
 
+app.use(cors());
 app.use(express.json());
 app.use('/api/auth/', authRouter);
 app.use('/api/notes/', notesRouter);
